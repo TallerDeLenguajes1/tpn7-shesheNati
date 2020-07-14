c8c0513c24cfe71ce6874239f6c30f7c117126c5
shesheNati <acosta_jessica@live.com>
2020-07-13 23:47:25 -0300

    calculadora finalizada

---
+++ calculadora1/calculadora1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora1
+{
+    public partial class Form1 : Form
+    {
+        float num1;
+        float num2;
+        string operador;
+        Calculadora NuevaCalculadora = new Calculadora();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boton7_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "9";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "6";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "1";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "2";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "3";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            pantalla.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            operador = "+";
+            num1 = float.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            operador = "-";
+            num1 = float.Parse(pantalla.Text);
+            pantalla.Clear();
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            operador = "*";
+            num1 = float.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            operador = "/";
+            num1 = float.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            num2 = float.Parse(pantalla.Text);
+            switch (operador)
+            {
+                case "+":
+                    pantalla.Clear();
+                    NuevaCalculadora.Numero1 = num1;
+                    NuevaCalculadora.Numero2 = num2;
+                   pantalla.Text = Convert.ToString(NuevaCalculadora.Sumar());
+                    break;
+                case "-":
+                    pantalla.Clear();
+                    NuevaCalculadora.Numero1 = num1;
+                    NuevaCalculadora.Numero2 = num2;
+                    pantalla.Text = Convert.ToString(NuevaCalculadora.Restar());
+                    break;
+                case "*":
+                    pantalla.Clear();
+                    NuevaCalculadora.Numero1 = num1;
+                    NuevaCalculadora.Numero2 = num2;
+                    pantalla.Text = Convert.ToString(NuevaCalculadora.Multiplicar());
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        pantalla.Text = "MATH ERROR!!"; break;
+                    }
+                    pantalla.Clear();
+                    NuevaCalculadora.Numero1 = num1;
+                    NuevaCalculadora.Numero2 = num2;
+                    pantalla.Text = Convert.ToString(NuevaCalculadora.Dividir());
+                    break;
+            }
+        }
+    }
+}
---
+++ tp7/tp7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
